e92f658d66f2a41457883fcf92a48d0e3c106408
Alex <nomadalex2020@gmail.com>
2021-12-24 15:20:25 +0800

    contracts: fix AssetProxy.

--- contracts/contracts/AssetProxy.sol
+++ contracts/contracts/AssetProxy.sol
@@ -6,6 +6,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IAssetProxy.sol";
 
 contract AssetProxy is IAssetProxy, Ownable {
+    receive() external payable {}
+
     function proxyCall(address _dst, bytes memory _calldata) external override onlyOwner {
         (bool success,) = _dst.delegatecall(_calldata);
         require(success);
